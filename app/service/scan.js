8e9b6302a562c15c899fde9f9ba501a30777e635
xank <xank@qq.com>
2018-11-04 01:50:51 +0800

    check

--- app/service/scan.js
+++ app/service/scan.js
@@ -5,7 +5,6 @@ module.exports = app => {
   class Service extends app.Service {
     async scan() {
       const data = await superagent.get('http://shanghaicity.openservice.kankanews.com/public/tour/filterinfo2');
-
       const list = data.body;
 
       const creates = [];
@@ -20,9 +19,7 @@ module.exports = app => {
           utime,
           date: utime.split(' ')[0],
         };
-
         creates.push(create);
-        // this.ctx.model.Flow.create(add);
       });
 
       if (!creates) {
@@ -30,18 +27,17 @@ module.exports = app => {
         return;
       }
 
-      // const utime = creates[0].utime;
-      // const lastData = await this.ctx.model.Flow.findOne({
-      //   order: [[ 'utime', 'DESC' ]],
-      // });
-
       // 判断记录时间是否重复或小于记录值
-      // if (lastData && utime <= lastData.utime) {
-      //   console.log(utime, '重复');
-      //   return;
-      // }
-      this.ctx.model.Flow.bulkCreate(creates);
+      const utime = creates[0].utime;
+      const lastData = await this.ctx.model.Flow.findOne({
+        order: [[ 'utime', 'DESC' ]],
+      });
+      if (lastData && utime <= lastData.utime) {
+        console.log(utime, '重复');
+        return;
+      }
 
+      this.ctx.model.Flow.bulkCreate(creates);
       console.log(utime, 'SUCCESS');
       return {
         lastUtime: lastData.utime,
